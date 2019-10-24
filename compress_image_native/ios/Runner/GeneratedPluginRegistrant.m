6ae9f05ce47ce08ad1341a27c7d0d4f8dc7dd4db
Daveat Corn <Condaveat@gmail.com>
2019-10-24 15:20:45 +0700

    Add files via upload
    compress image

---
+++ compress_image_native/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <flutter_image_compress/FlutterImageCompressPlugin.h>
+#import <flutter_native_image/FlutterNativeImagePlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterImageCompressPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterImageCompressPlugin"]];
+  [FlutterNativeImagePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNativeImagePlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+}
+
+@end
